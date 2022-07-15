19bd1577cfde9175b51f2b1b161423dd453d288e
nadinefiona <nadinefiona9@gmail.com>
2022-07-15 09:48:58 +0200

    making some changes

---
+++ exercises/tst.js
@@ -0,0 +1,55 @@
+            // //create variable
+            // var fname = "Nadine Fiona"
+            // var lname = "BYUKUSENGE"
+            // var salary = 2000;
+
+            // var vat = 0.34
+
+            // var net = salary * vat;
+
+    
+            // var decision = (net<2000)?"undersalary":"oversalary"
+
+
+            // // Display statements
+            // document.write(fname+" <br> "+lname+"<br> with salary: "+net+ "frw")
+            // document.write("<h1>hello js</h1>")
+            // console.log("hiiiiiiiiiii")
+            // console.log(fname+"\n"+lname+"\nwith salary:"+net+ "frw")
+
+
+
+
+
+
+            
+            // var d = new Date();
+            // theDay = d.getDay();
+            // document.write(d)
+            // switch(theDay)
+            // {
+            //     case 0:
+            //         document.write("Finally Friday");
+            //         break;
+            //         case 1:
+            //             document.write("Super Saturday");
+            //             break;
+            //             case 2:
+            //                 document.write("Sleepy Sunday");
+            //                 break;
+            //                 default:
+            //                     document.write("I am looking to this weekend!");
+            // }
+
+
+            
+           
+            
+var i = 0;
+do{
+    document.write("the number is" +i);
+    document.write("<br/>");
+
+    i++;
+}
+while(i<=5);
